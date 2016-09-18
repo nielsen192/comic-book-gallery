53e1deb96e959e7bda4d8bbafc9e0c709940fad7
Thomas Vest Nielsen <nielsen192@gmail.com>
2016-09-18 11:51:46 +0200

    Added Comic Book and Artist models

---
+++ src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+    }
+}
